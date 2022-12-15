93a6c5184049a1ac4b2f115e23da5160e057e0cd
gantalikhitha <105198094+gantalikhitha@users.noreply.github.com>
2022-12-15 15:16:02 +0530

    Update JenkinsDemoApplication.java

--- src/main/java/com/jenkins/JenkinsDemoApplication.java
+++ src/main/java/com/jenkins/JenkinsDemoApplication.java
@@ -2,10 +2,15 @@ package com.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsDemoApplication {
-
+public class JenkinsDemoApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder initializer) {
+		return initializer.sources(JenkinsDemoApplication.class);
+}
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
